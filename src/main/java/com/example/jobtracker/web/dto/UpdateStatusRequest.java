8c38009ab9f154cb1a4c9ec5a52bb00a6c749633
h669832 <669832@stud.hvl.no>
2026-01-22 16:38:13 +0100

    lagt til

--- src/main/java/com/example/jobtracker/web/dto/UpdateStatusRequest.java
+++ src/main/java/com/example/jobtracker/web/dto/UpdateStatusRequest.java
@@ -4,5 +4,6 @@ public class UpdateStatusRequest {
     private String status; // "SOKT", "INTERVJU" ...
 
     public String getStatus() { return status; }
+
     public void setStatus(String status) { this.status = status; }
 }
